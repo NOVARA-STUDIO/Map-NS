c524bc46fb266b97a3f2fee58eaa638e27891201
NOVARA STUDIO™ <missbordug09@gmail.com>
2026-07-12 14:56:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const MAP_CONFIG = {
   edgeFade: true,
   fadeWidth: 50,    
   
-  rows: 5,
-  cols: 5,
+  rows: 4,
+  cols: 4,
 
   tiles: [
     { row: 0, col: 0, src: "photos/0_0.jpg" },
@@ -16,41 +16,31 @@ const MAP_CONFIG = {
     { row: 0, col: 2, src: "photos/0_2.jpg" },
     { row: 0, col: 3, src: "photos/0_3.jpg" },
     { row: 0, col: 4, src: "photos/0_4.jpg" },
-    { row: 0, col: 5, src: "photos/0_5.jpg" },
 
     { row: 1, col: 0, src: "photos/1_0.jpg" },
     { row: 1, col: 1, src: "photos/1_1.jpg" },
     { row: 1, col: 2, src: "photos/1_2.jpg" },
     { row: 1, col: 3, src: "photos/1_3.jpg" },
     { row: 1, col: 4, src: "photos/1_4.jpg" },
-    { row: 1, col: 5, src: "photos/1_5.jpg" },
 
     { row: 2, col: 0, src: "photos/2_0.jpg" },
     { row: 2, col: 1, src: "photos/2_1.jpg" },
     { row: 2, col: 2, src: "photos/2_2.jpg" },
     { row: 2, col: 3, src: "photos/2_3.jpg" },
     { row: 2, col: 4, src: "photos/2_4.jpg" },
-    { row: 2, col: 5, src: "photos/2_5.jpg" },
 
     { row: 3, col: 0, src: "photos/3_0.jpg" },
     { row: 3, col: 1, src: "photos/3_1.jpg" },
     { row: 3, col: 2, src: "photos/3_2.jpg" },
     { row: 3, col: 3, src: "photos/3_3.jpg" },
     { row: 3, col: 4, src: "photos/3_4.jpg" },
-    { row: 3, col: 5, src: "photos/3_5.jpg" },
 
     { row: 4, col: 0, src: "photos/4_0.jpg" },
     { row: 4, col: 1, src: "photos/4_1.jpg" },
     { row: 4, col: 2, src: "photos/4_2.jpg" },
     { row: 4, col: 3, src: "photos/4_3.jpg" },
     { row: 4, col: 4, src: "photos/4_4.jpg" },
-    { row: 4, col: 5, src: "photos/4_5.jpg" },
 
-    { row: 5, col: 0, src: "photos/5_0.jpg" },
-    { row: 5, col: 1, src: "photos/5_1.jpg" },
-    { row: 5, col: 2, src: "photos/5_2.jpg" },
-    { row: 5, col: 3, src: "photos/5_3.jpg" },
-    { row: 5, col: 4, src: "photos/5_4.jpg" },
-    { row: 5, col: 5, src: "photos/5_5.jpg" },
+
   ],
 };
